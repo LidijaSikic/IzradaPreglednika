4c2e46f5f0a28bcf21d26b09675ccb8984ae3cf9
Lidija Sikic <ligalic@vsite.hr>
2020-06-12 17:02:06 +0200

    Add project files.

---
+++ IzradaPreglednika/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace IzradaPreglednika
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(12, 12);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "<<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(107, 11);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = ">>";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(199, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(290, 12);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(75, 23);
+            this.btnHome.TabIndex = 3;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(383, 15);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(370, 20);
+            this.txtUrl.TabIndex = 4;
+            this.txtUrl.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUrl_KeyPress);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(759, 13);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 538);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(3, 65);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(999, 470);
+            this.webBrowser1.TabIndex = 7;
+            this.webBrowser1.Url = new System.Uri("http://www.vsite.hr", System.UriKind.Absolute);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 538);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1002, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(156, 17);
+            this.toolStripStatusLabel1.Text = "Stranica je uspješno učitana!";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 560);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnBack);
+            this.Name = "Form1";
+            this.Text = "Preglednik";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ IzradaPreglednika/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IzradaPreglednika
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            webBrowser1.DocumentCompleted += (o, e) =>
+            {
+                toolStripStatusLabel1.Text = "Stranica je uspješno učitana";
+            };
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Stranica se učitava";
+            webBrowser1.GoHome();
+           
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Stranica se učitava";
+
+            if (String.IsNullOrEmpty(txtUrl.Text) || txtUrl.Text.Equals("about:blank"))
+            {
+                toolStripStatusLabel1.Text = "Greška";
+                MessageBox.Show("Enter a valid URL.");
+                txtUrl.Focus();
+                return;
+            }
+            
+            OpenURLInBrowser(txtUrl.Text);
+          
+        }
+
+        private void txtUrl_KeyPress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                toolStripStatusLabel1.Text = "Stranica se učitava";
+
+                string url = UrediUnos(txtUrl.Text);
+
+                if (ProvjeraUnosa(url))
+                {
+
+                    OpenURLInBrowser(url);
+                 
+                }
+
+            }
+        }
+
+        private void OpenURLInBrowser(string url)
+        {
+            toolStripStatusLabel1.Text = "Stranica se učitava";
+
+            try
+                {
+                    webBrowser1.Navigate(new Uri(url));
+                    
+            }
+                catch (System.UriFormatException)
+                {
+                toolStripStatusLabel1.Text = "Greška";
+                return;
+                }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoBack)
+                toolStripStatusLabel1.Text = "Stranica se učitava";
+                webBrowser1.GoBack();
+               
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+                toolStripStatusLabel1.Text = "Stranica se učitava";
+            webBrowser1.GoForward();
+            
+        }
+
+        
+        
+
+        private string UrediUnos(string url)
+        {
+           
+            if (!url.StartsWith("https://www.") && !url.StartsWith("https://www."))
+            {
+                url = "http://www." + url;
+            }
+            else if (!url.StartsWith("http://") && !url.StartsWith("https://"))
+            {
+                url = "http://" + url;
+            }
+
+            return url;
+            
+        }
+
+        private bool ProvjeraUnosa(string url)
+        {
+            int count;
+            if (String.IsNullOrEmpty(url) || url.Equals("about:blank"))
+            {
+                toolStripStatusLabel1.Text = "Greška";
+                MessageBox.Show("No URL!!!");
+                txtUrl.Focus();               
+                return false;
+            }
+            else if (!((count = url.Count(c => c == '.')) == 2))
+            {
+                toolStripStatusLabel1.Text = "Greška";
+                MessageBox.Show("Not a valid URL.");
+                txtUrl.Focus();               
+                return false;
+            }
+
+            Uri adresa;
+            if (Uri.TryCreate(url, UriKind.Absolute, out adresa))
+            {
+                return true;
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "Greška";
+                MessageBox.Show("Bad Url!");
+                return false;
+
+            }
+
+          
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Prekid učitavanja";
+            webBrowser1.Stop();
+        }
+
+      
+    }
+}
